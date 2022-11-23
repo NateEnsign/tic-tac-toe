591d032a061efc3ffa4c888d74a0c21f6a6066b4
Nate Ensign <dixienate@gmail.com>
2022-11-22 19:30:48 -0700

    complete through part 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Square from "./Square";
+
+function App() {
+  const basketball = 'Ankles Broken'
+
+  return (
+    <div className="App">
+      <Square kobe={basketball}/>
+    </div>
+  );
+}
+
+export default App;
